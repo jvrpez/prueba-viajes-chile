5b3073c7679e6785e8834de700f0b9b9515847e4
Javiera López <javieripilm@gmail.com>
2022-05-10 13:32:50 -0400

    Sexto Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+//Carousel JS
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+//Tooltip JS
+//var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//return new bootstrap.Tooltip(tooltipTriggerEl)
+//})
